73770f4a4d74bbf68abfdd0744da34fa1cd34d32
Pedrisbel <pedrisbellh@uci.cu>
2025-01-22 23:23:14 -0500

    Refactor routing paths and screens
    This commit refactors the routing paths and screens in the main.dart file. It adds new routes for the CollectScreen, ModifyEmailScreen, ModifyPasswordScreen, and AppDetailsScreen. It also updates the existing routes for the CreateLoanScreen, ViewLoanScreen, ClientDetailsScreen, ViewCompanyScreen, UserPanelScreen, and PrintScreen. This refactoring improves the organization and navigation of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,14 @@ import 'package:prestamos/ui/company/bloc/company_bloc.dart';
 import 'package:prestamos/ui/company/screens/create_company_screen.dart';
 import 'package:prestamos/ui/home/home_screen.dart';
 import 'package:prestamos/ui/loan/bloc/loan_bloc.dart';
+import 'package:prestamos/ui/loan/screens/collect_screen.dart';
 import 'package:prestamos/ui/loan/screens/create_loan_screen.dart';
 import 'package:prestamos/ui/loan/screens/delays_screen.dart';
 import 'package:prestamos/ui/loan/screens/loans_screen.dart';
+import 'package:prestamos/ui/user/screens/modify_email_screen.dart';
 import 'package:prestamos/ui/loan/screens/print_screen.dart';
+import 'package:prestamos/ui/user/screens/app_details_screen.dart';
+import 'package:prestamos/ui/user/screens/modify_password_screen.dart';
 import 'package:prestamos/ui/user/screens/register_screen.dart';
 import 'package:prestamos/ui/user/screens/settings_screen.dart';
 import 'package:prestamos/ui/user/screens/user_panel_screen.dart';
@@ -74,91 +78,87 @@ final GoRouter _router = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomeScreen();
-      },
+      builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/login',
-      builder: (BuildContext context, GoRouterState state) {
-        return const LoginScreen();
-      },
+      builder: (context, state) => const LoginScreen(),
     ),
     GoRoute(
       path: '/register',
-      builder: (BuildContext context, GoRouterState state) {
-        return const RegisterScreen();
-      },
+      builder: (context, state) => const RegisterScreen(),
     ),
     GoRoute(
       path: '/clients',
-      builder: (BuildContext context, GoRouterState state) {
-        return const ClientsScreen();
-      },
+      builder: (context, state) => const ClientsScreen(),
     ),
     GoRoute(
       path: '/loans',
-      builder: (BuildContext context, GoRouterState state) {
-        return const LoansScreen();
-      },
+      builder: (context, state) => const LoansScreen(),
+    ),
+    GoRoute(
+      path: '/toCollect',
+      builder: (context, state) => const CollectScreen(),
     ),
     GoRoute(
       path: '/delays',
-      builder: (BuildContext context, GoRouterState state) {
-        return const DelaysScreen();
-      },
+      builder: (context, state) => const DelaysScreen(),
     ),
     GoRoute(
       path: '/settings',
-      builder: (BuildContext context, GoRouterState state) {
-        return const SettingsScreen();
-      },
+      builder: (context, state) => const SettingsScreen(),
+    ),
+    GoRoute(
+      path: '/modifyEmail',
+      builder: (context, state) => const ModifyEmailScreen(),
+    ),
+    GoRoute(
+      path: '/modifyPassword',
+      builder: (context, state) => const ModifyPasswordScreen(),
+    ),
+    GoRoute(
+      path: '/appDetails',
+      builder: (context, state) => const AppDetailsScreen(),
     ),
     GoRoute(
       path: '/createLoan/:clientName',
-      builder: (BuildContext context, GoRouterState state) {
+      builder: (context, state) {
         final String clientName = state.pathParameters['clientName']!;
         return CreateLoanScreen(clientName: clientName);
       },
     ),
     GoRoute(
       path: '/viewLoan/:loanId',
-      builder: (BuildContext context, GoRouterState state) {
+      builder: (context, state) {
         final String loanId = state.pathParameters['loanId']!;
         return ViewLoanScreen(loanId: loanId);
       },
     ),
     GoRoute(
       path: '/clientDetails/:clientId',
-      builder: (BuildContext context, GoRouterState state) {
+      builder: (context, state) {
         final String clientId = state.pathParameters['clientId']!;
         return ClientDetailsScreen(clientId: clientId);
       },
     ),
     GoRoute(
       path: '/createCompany',
-      builder: (BuildContext context, GoRouterState state) {
-        return const CreateCompanyScreen();
-      },
+      builder: (context, state) => const CreateCompanyScreen(),
     ),
     GoRoute(
       path: '/viewCompany/:userId',
-      builder: (BuildContext context, GoRouterState state) {
+      builder: (context, state) {
         final String userId = state.pathParameters['userId']!;
         return ViewCompanyScreen(userId: userId);
       },
     ),
     GoRoute(
       path: '/userPanel',
-      builder: (BuildContext context, GoRouterState state) {
-        return const UserPanelScreen();
-      },
+      builder: (context, state) => const UserPanelScreen(),
     ),
     GoRoute(
       path: '/print',
-      builder: (BuildContext context, GoRouterState state) {
-        return const PrintScreen();
-      },
+      builder: (context, state) => const PrintScreen(),
     ),
   ],
   errorBuilder: (context, state) {
