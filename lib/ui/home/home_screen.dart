e6f82aac2c4acec0f57a874694bbd6d1de3bc581
Pedrisbel <pedrisbellh@uci.cu>
2025-01-22 23:22:39 -0500

    Refactor home screen layout and functionality

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,9 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:prestamos/data/providers/company_provider/company_provider_impl.dart';
-import 'package:prestamos/data/repositories/company_repository.dart';
-import 'package:prestamos/domain/models/company/company.dart';
 import 'package:prestamos/ui/extensions/build_context_extension.dart';
 import 'package:prestamos/ui/widgets/utils/snack_bar_top.dart';
 
@@ -15,7 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  late CompanyRepository companyRepository;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   String? userId;
 
@@ -23,7 +19,6 @@ class HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     userId = _auth.currentUser?.uid;
-    companyRepository = CompanyRepository(CompanyProviderImpl());
   }
 
   Future<void> _logout(BuildContext context) async {
@@ -47,41 +42,6 @@ class HomeScreenState extends State<HomeScreen> {
         title: const Text('PayMe'),
         backgroundColor: Colors.teal,
         foregroundColor: Colors.white,
-        actions: [
-          PopupMenuButton<int>(
-            icon: const Icon(Icons.more_vert),
-            offset: const Offset(0, 52),
-            onSelected: (value) async {
-              switch (value) {
-                // Cual es la opcion (1)
-                // llamo a un evento del bloc
-                case 1:
-                  String userId = FirebaseAuth.instance.currentUser!.uid;
-                  Company? company = await companyRepository.getCompany(userId);
-
-                  if (company != null) {
-                    context.push('/viewCompany/$userId');
-                  } else {
-                    context.push('/createCompany');
-                  }
-                  break;
-                case 2:
-                  context.push('/userPanel');
-                  break;
-              }
-            },
-            itemBuilder: (context) => [
-              PopupMenuItem<int>(
-                value: 1,
-                child: Text(context.l10n.viewCompany),
-              ),
-              PopupMenuItem<int>(
-                value: 2,
-                child: Text(context.l10n.accountDetails),
-              ),
-            ],
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -89,14 +49,20 @@ class HomeScreenState extends State<HomeScreen> {
           crossAxisCount: 2,
           childAspectRatio: 1.5,
           children: [
-            _buildGridItem(Icons.person, 'Clientes', context, '/clients'),
+            _buildGridItem(Icons.person, 'Clientes', context, '/clients',
+                color: Colors.teal),
             _buildGridItem(
-                Icons.monetization_on, 'Préstamos', context, '/loans'),
+                Icons.monetization_on, 'Préstamos', context, '/loans',
+                color: Colors.teal),
+            _buildGridItem(
+                Icons.date_range_outlined, 'Cobrar', context, '/toCollect',
+                color: Colors.teal),
             _buildGridItem(
                 Icons.date_range_outlined, 'Atrasados', context, '/delays',
                 color: Colors.red.shade900),
             _buildGridItem(
-                Icons.settings, 'Configuración', context, '/settings'),
+                Icons.settings, 'Configuración', context, '/settings',
+                color: Colors.teal),
             _buildGridItem(Icons.logout, 'Cerrar Sesión', context, '',
                 color: Colors.red.shade900, onTap: () => _logout(context)),
           ],
